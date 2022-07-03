9ece3a2e20144c89b55d166edc4a2e4f57c5c474
K1mimaru <kimimarusadboy01@gmail.com>
2022-07-03 08:22:02 +0000

    update dias.js

--- dias.js
+++ dias.js
@@ -212,6 +212,57 @@ throw `Kirim Gambar/Video Dengan Caption ${prefix + command}\nDurasi Video 1-9 D
 }
 }
 break
+case 'play': case 'ytplay': {
+if (!text) throw `Example : ${prefix + command} story wa anime`
+
+let yts = require("yt-search")
+let search = await yts(text)
+let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
+let buttons = [
+{buttonId: `ytmp3 ${anu.url}`, buttonText: {displayText: 'A U D I O'}, type: 1},
+{buttonId: `ytmp4 ${anu.url}`, buttonText: {displayText: 'V I D E O'}, type: 1}
+]
+let buttonMessage = {
+image: { url: anu.thumbnail },
+caption: `
+⭓ Title : ${anu.title}
+⭓ Ext : Search
+⭓ ID : ${anu.videoId}
+⭓ Duration : ${anu.timestamp}
+⭓ Viewers : ${anu.views}
+⭓ Upload At : ${anu.ago}
+⭓ Author : ${anu.author.name}
+⭓ Channel : ${anu.author.url}
+⭓ Description : ${anu.description}
+⭓ Url : ${anu.url}`,
+footer: kimimaru.user.name,
+buttons: buttons,
+headerType: 4
+}
+kimimaru.sendMessage(m.chat, buttonMessage, { quoted: m })
+}
+break
+case 'ytmp3': case 'ytaudio': {
+let { yta } = require('./lib/y2mate')
+if (!text) throw `Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 128kbps`
+
+let quality = args[1] ? args[1] : '128kbps'
+let media = await yta(text, quality)
+if (media.filesize >= 1000000) return reply('File Melebihi Batas '+util.format(media))
+kimimaru.sendImage(m.chat, media.thumb, `⭓ Title : ${media.title}\n⭓ File Size : ${media.filesizeF}\n⭓ Url : ${isUrl(text)}\n⭓ Ext : MP3\n⭓ Resolusi : ${args[1] || '128kbps'}`, m)
+kimimaru.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'audio/mpeg', fileName: `${media.title}.mp3` }, { quoted: m })
+}
+break
+case 'ytmp4': case 'ytvideo': {
+let { ytv } = require('./lib/y2mate')
+if (!text) throw `Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 360p`
+
+let quality = args[1] ? args[1] : '360p'
+let media = await ytv(text, quality)
+if (media.filesize >= 100000) return reply('File Melebihi Batas '+util.format(media))
+kimimaru.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `⭓ Title : ${media.title}\n⭓ File Size : ${media.filesizeF}\n⭓ Url : ${isUrl(text)}\n⭓ Ext : MP3\n⭓ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
+}
+break
 case 'stickerwm': case 'swm': case 'stikerwm': {
 if (!quoted) throw `Balas Video/Image Dengan Caption ${prefix + command} packname|author`
 if (!text) throw `Example: ${prefix + command} packname|author`
@@ -264,6 +315,10 @@ case 'menu-all': {
 │• ${prefix}stiker
 │• ${prefix}stikerwm
 │• ${prefix}take
+│
+│• ${prefix}play
+│• ${prefix}ytmp3
+│• ${prefix}ytmp4
 └───────────┈ ⳹`
 	 let buttons = [
         { buttonId: `${prefix}owner`, buttonText: { displayText: `OWNER DZXYBOT` }, type: 1 }
